5bdf4ceefad422033e9c57137e9481533000850d
Jackson <jacksonboss88@gmail.com>
2024-12-24 17:28:25 -0600

    Created add-task form with html

--- src/index.js
+++ src/index.js
@@ -1,10 +1,1 @@
 import "./styles.css";
-import {createTodos} from "./home.js";
-import {taskDom} from "./task.js";
-
-(() => {
-    const taskBtn = document.querySelector('.add-task');
-    taskBtn.addEventListener('click', () => {
-        taskDom();
-    })
-})();
