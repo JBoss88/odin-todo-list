88688683616ef4e98c3075e1bcf5e32a2ab8cb72
Jackson <jacksonboss88@gmail.com>
2025-01-03 16:36:35 -0600

    Finished sidebar

--- src/home.js
+++
@@ -1,9 +0,0 @@
-export const makeDefaultDom = (() => {
-    const defaultContainer = document.createElement('div');
-    defaultContainer.classList.add('default-container');
-
-    const defaultTitle = document.createElement('h1');
-    defaultTitle.classList.add('default-title');
-    defaultTitle.textContent = "Default Project"
-
-})();
--- src/index.js
+++ src/index.js
@@ -1,7 +1,1 @@
 import "./styles.css";
-
-import {changeTaskVisibility, submitForm} from "./task.js";
-import { makeDefaultDom } from "./home.js";
-
-changeTaskVisibility();
-submitForm();
